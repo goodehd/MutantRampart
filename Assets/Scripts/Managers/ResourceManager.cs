080abe3983b7be11303e67c115db255cc720e3eb
ZANE <130233619+chochozane@users.noreply.github.com>
2024-01-16 21:54:20 +0900

    [EDIT] ResourceManager.cs
    기존에 중복되는 부분들 제네릭을 통해 코드 re작성

--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -1,82 +1,91 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class ResourceManager : IManagers
 {
     // 각 Assets 들을 Dictionary 로 관리
-    private Dictionary<string, Sprite> _sprites = new Dictionary<string, Sprite>();
-    private Dictionary<string, GameObject> _prefabs = new Dictionary<string, GameObject>();
+    private Dictionary<string, Object> _resources = new Dictionary<string, Object>();
+    //private Dictionary<string, Sprite> _sprites = new Dictionary<string, Sprite>();
+    //private Dictionary<string, GameObject> _prefabs = new Dictionary<string, GameObject>();
 
-    private Dictionary<string, Object> _resource = new Dictionary<string, Object>();
 
-
-    // 초기화 과정에서 LoadAll
+    // 초기화 과정에서 Load 또는 LoadAll
     public bool Init()
     {
-        Sprite[] sprites = Resources.LoadAll<Sprite>(""); // todo : 경로에서 sprites 다 델꼬와
-        foreach (Sprite sprite in sprites)
-        {
-            _sprites.Add(sprite.name, sprite);
-        }
+        //Sprite[] sprites = Resources.LoadAll<Sprite>(""); // todo : 경로에서 sprites 다 델꼬와
+        //foreach (Sprite sprite in sprites)
+        //{
+        //    _sprites.Add(sprite.name, sprite);
+        //}
 
-        GameObject[] objs = Resources.LoadAll<GameObject>(""); // todo : 경로에서 prefab 대상 델꼬와
-        foreach (GameObject obj in objs)
-        {
-            _prefabs.Add(obj.name, obj);
-        }
+        //GameObject[] objs = Resources.LoadAll<GameObject>(""); // todo : 경로에서 prefab 대상 델꼬와
+        //foreach (GameObject obj in objs)
+        //{
+        //    _prefabs.Add(obj.name, obj);
+        //}
+
+        // 이런 식으로 Init 에서 필요한 것들 Load 관련 함수 사용해서 불러오면 되는건가
+        Load<SpriteRenderer>("");
+        LoadAll<GameObject>("");
 
         return true;
     }
 
-    public Sprite LoadSprite(string key)
+    // Load - 배열아닌 ver.
+    public T Load<T>(string path) where T : Object
     {
-        // TryGetValue - Dictionary 안에 해당 key 가 있는지 확인하고, 있으면 sprite 리턴, 없으면 false 리턴.
-        // TryGetValue 는 key 가 없는 경우에 false 를 리턴해주면서 최대한 에러 뜨는 걸 최소화(?)시켜준다고 보면 된다.
-        if (!_sprites.TryGetValue(key, out Sprite sprite))
-        {
-            Debug.LogError($"[ResourceManager] LoadSprite({key}) : Failed to load sprite.");
-            return null;
-        }
-
-        return sprite;
+        T resource = Resources.Load<T>(path);
+        _resources.Add(resource.name, resource);
+        return resource;
     }
 
-    public GameObject LoadPrefab(string key)
-    {
-        if (!_prefabs.TryGetValue(key, out GameObject prefab))
-        {
-            Debug.LogError($"[ResourceManager] LoadPrefab({key}) : Failed to load prefab.");
-            return null;
-        }
-
-        return prefab;
-    }
-
-    public T LoadResource<T>(string key) where T : Object
-    {
-        if (!_resource.TryGetValue(key, out Object resource))
-        {
-            Debug.LogError($"[ResourceManager] LoadPrefab({key}) : Failed to load prefab.");
-            return null;
-        }
-        return resource as T;
-    }
-
+    // Load - 배열 ver.
     public T[] LoadAll<T>(string path) where T : Object
     {
         T[] resources = Resources.LoadAll<T>(path);
-        foreach(Object obj in resources)
+        foreach (T resource in resources) // T 대신 Object 도 가능.
         {
-            _resource.Add(obj.name, obj);
+            _resources.Add(resource.name, resource);
         }
         return resources;
     }
 
-    public T Load<T>(string path) where T : Object
+    //public Sprite LoadSprite(string key)
+    //{
+    //    // TryGetValue - Dictionary 안에 해당 key 가 있는지 확인하고, 있으면 sprite 리턴, 없으면 false 리턴.
+    //    // TryGetValue 는 key 가 없는 경우에 false 를 리턴해주면서 최대한 에러 뜨는 걸 최소화(?)시켜준다고 보면 된다.
+    //    if (!_sprites.TryGetValue(key, out Sprite sprite))
+    //    {
+    //        Debug.LogError($"[ResourceManager] LoadSprite({key}) : Failed to load sprite.");
+    //        return null;
+    //    }
+
+    //    return sprite;
+    //}
+
+    //public GameObject LoadPrefab(string key)
+    //{
+    //    if (!_prefabs.TryGetValue(key, out GameObject prefab))
+    //    {
+    //        Debug.LogError($"[ResourceManager] LoadPrefab({key}) : Failed to load prefab.");
+    //        return null;
+    //    }
+
+    //    return prefab;
+    //}
+
+    public T LoadResource<T>(string key) where T : Object
     {
-        T resource = Resources.Load<T>(path);
-        _resource.Add(resource.name, resource);
-        return resource;
+        // TryGetValue - Dictionary 안에 해당 key 가 있는지 확인하고, 있으면 sprite 리턴, 없으면 false 리턴.
+        // TryGetValue 는 key 가 없는 경우에 false 를 리턴해주면서 최대한 에러 뜨는 걸 최소화(?)시켜준다고 보면 된다.
+        if (!_resources.TryGetValue(key, out Object resource))
+        {
+            Debug.LogError($"[ResourceManager] LoadResource({key}) : Failed to load resource.");
+            return null;
+        }
+        return resource as T;
     }
 
     /// <summary>
@@ -91,7 +100,7 @@ public class ResourceManager : IManagers
         GameObject prefab = LoadResource<GameObject>(key);
         if (prefab == null)
         {
-            Debug.LogError($"[ResourceManager] InstantiateWithPoolingOption({key}) : Failed to load prefab.");
+            Debug.LogError($"[ResourceManager] InstantiateWithPoolingOption({key}) : Failed to load resource_prefab.");
             return null;
         }
 
@@ -100,7 +109,7 @@ public class ResourceManager : IManagers
             return Main.Get<PoolManager>().Pop(prefab);
         }
 
-        // pooling 이 false 면 pooling 이 적용 안 된 Instantiate 진행 !
+        // pooling 이 false 면 pooling 옵션 없는 Instantiate 진행 !
         GameObject obj = Object.Instantiate(prefab, parent);
         obj.name = prefab.name;
         return obj;
