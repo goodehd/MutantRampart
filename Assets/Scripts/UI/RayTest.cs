dbbe8598dcc7aab595aae9e4c29ed97c431fd909
dbswls2114 <54023919+dbswls2114@users.noreply.github.com>
2024-01-19 10:32:42 +0900

    [Fix] Room transform fix
    z축을 기준으로 타일맵 설정

---
+++ Assets/Scripts/UI/RayTest.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTest : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log(gameObject.name);
+    }
+    
+}
