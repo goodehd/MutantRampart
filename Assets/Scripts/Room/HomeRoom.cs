2bd93b17f48b33a78b7ac25ee45139b183d3284b
goodehd <75984454+goodehd@users.noreply.github.com>
2024-01-24 20:38:29 +0900

    [fix] roomdata fix

--- Assets/Scripts/Room/HomeRoom.cs
+++ Assets/Scripts/Room/HomeRoom.cs
@@ -7,15 +7,15 @@ public class HomeRoom : BatRoom
     public int damageAmount = 10;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy")) // 적 오브젝트와 충돌했을 때
-        {
-            if (!_isUnitAlive) // Home 안의 유닛이 살아있지 않다면
-            {
-                // 플레이어의 Hp 감소
-                Main.Get<GameManager>().PlayerHp -= damageAmount;
-                Destroy(other.gameObject);
-            }
-            // 여기에서 필요한 다른 작업 수행 가능
-        }
+        //if (other.CompareTag("Enemy")) // 적 오브젝트와 충돌했을 때
+        //{
+        //    if (!_isUnitAlive) // Home 안의 유닛이 살아있지 않다면
+        //    {
+        //        // 플레이어의 Hp 감소
+        //        Main.Get<GameManager>().PlayerHp -= damageAmount;
+        //        Destroy(other.gameObject);
+        //    }
+        //    // 여기에서 필요한 다른 작업 수행 가능
+        //}
     }
 }
