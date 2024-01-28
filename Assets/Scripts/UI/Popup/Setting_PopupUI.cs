661bbb308c44642da0d29e0cd2c899830f305880
ZANE <130233619+chochozane@users.noreply.github.com>
2024-01-28 17:32:28 +0900

    [ADD] Script - NightMain_SceneUI

--- Assets/Scripts/UI/Popup/Setting_PopupUI.cs
+++ Assets/Scripts/UI/Popup/Setting_PopupUI.cs
@@ -7,49 +7,67 @@ using UnityEngine.UI;
 public class Setting_PopupUI : BaseUI
 {
     private Button _settingCloseBtn;
-    // slider 는 UnityEngine.UI
-    private Slider _bgmSlider;
-    private Slider _effectSlider;
-    private Slider _uiSlider;
     private Button _bgmMuteBtn;
     private Button _bgmMaxBtn;
     private Button _effectMuteBtn;
     private Button _effectMaxBtn;
     private Button _uiMuteBtn;
     private Button _uiMaxBtn;
+    private Button _languageBtn;
     private Button _guideBtn;
     private Button _usBtn;
 
+    private Slider _bgmSlider; // slider 는 UnityEngine.UI
+    private Slider _effectSlider;
+    private Slider _uiSlider;
+
     protected override void Init()
     {
+        SetButton();
+        SetSlider();
+    }
+
+    private void SetButton()
+    {
         SetUI<Button>();
-        SetUI<Slider>();
 
         _settingCloseBtn = GetUI<Button>("SettingCloseBtn");
+        _bgmMuteBtn = GetUI<Button>("BGMMuteBtn");
+        _bgmMaxBtn = GetUI<Button>("BGMMaxBtn");
+        _effectMuteBtn = GetUI<Button>("EffectMuteBtn");
+        _effectMaxBtn = GetUI<Button>("EffectMaxBtn");
+        _uiMuteBtn = GetUI<Button>("UIMuteBtn");
+        _uiMaxBtn = GetUI<Button>("UIMaxBtn");
+        _languageBtn = GetUI<Button>("LangBtn");
+        _guideBtn = GetUI<Button>("GuideBtn");
+        _usBtn = GetUI<Button>("UsBtn");
+
         SetUICallback(_settingCloseBtn.gameObject, EUIEventState.Click, ClickCloseBtn);
+        SetUICallback(_bgmMuteBtn.gameObject, EUIEventState.Click, ClickBGMMuteBtn);
+        SetUICallback(_bgmMaxBtn.gameObject, EUIEventState.Click, ClickBGMMaxBtn);
+        SetUICallback(_effectMuteBtn.gameObject, EUIEventState.Click, ClickEffectMuteBtn);
+        SetUICallback(_effectMaxBtn.gameObject, EUIEventState.Click, ClickEffectMaxBtn);
+        SetUICallback(_uiMuteBtn.gameObject, EUIEventState.Click, ClickUIMuteBtn);
+        SetUICallback(_uiMaxBtn.gameObject, EUIEventState.Click, ClickUIMaxBtn);
+        SetUICallback(_languageBtn.gameObject, EUIEventState.Click, ClickLanguageBtn);
+        SetUICallback(_guideBtn.gameObject, EUIEventState.Click, ClickGuideBtn);
+        SetUICallback(_usBtn.gameObject, EUIEventState.Click, ClickUsBtn);
+    }
+
+    private void SetSlider()
+    {
+        SetUI<Slider>();
         _bgmSlider = GetUI<Slider>("BGM_Slider");
         _effectSlider = GetUI<Slider>("Effect_Slider");
         _uiSlider = GetUI<Slider>("UI_Slider");
-        _bgmMuteBtn = GetUI<Button>("BGMMuteBtn");
-        SetUICallback(_bgmMuteBtn.gameObject, EUIEventState.Click, ClickBGMMuteBtn);
-        _bgmMaxBtn = GetUI<Button>("BGMMaxBtn");
-        SetUICallback(_bgmMaxBtn.gameObject, EUIEventState.Click, ClickBGMMaxBtn);
-        _effectMuteBtn = GetUI<Button>("EffectMuteBtn");
-        SetUICallback(_effectMuteBtn.gameObject, EUIEventState.Click, ClickEffectMuteBtn);
-        _effectMaxBtn = GetUI<Button>("EffectMaxBtn");
-        SetUICallback(_effectMaxBtn.gameObject, EUIEventState.Click, ClickEffectMaxBtn);
-        _uiMuteBtn = GetUI<Button>("UIMuteBtn");
-        SetUICallback(_uiMuteBtn.gameObject, EUIEventState.Click, ClickUIMuteBtn);
-        _uiMaxBtn = GetUI<Button>("UIMaxBtn");
-        SetUICallback(_uiMaxBtn.gameObject, EUIEventState.Click, ClickUIMaxBtn);
-        _guideBtn = GetUI<Button>("GuideBtn");
-        SetUICallback(_guideBtn.gameObject, EUIEventState.Click, ClickGuideBtn);
-        _usBtn = GetUI<Button>("UsBtn");
-        SetUICallback(_usBtn.gameObject, EUIEventState.Click, ClickUsBtn);
-
     }
 
     // todo : slider들(사운드 관련) value 값 조절
+
+    private void ClickCloseBtn(PointerEventData EventData)
+    {
+        Main.Get<UIManager>().ClosePopup();
+    }
 
     private void ClickBGMMuteBtn(PointerEventData EventData)
     {
@@ -79,16 +97,19 @@ public class Setting_PopupUI : BaseUI
 
     }
 
+    private void ClickLanguageBtn(PointerEventData EventData)
+    {
+
+    }
+
     private void ClickGuideBtn(PointerEventData EventData)
     {
 
     }
+
     private void ClickUsBtn(PointerEventData EventData)
     {
 
     }
-    private void ClickCloseBtn(PointerEventData EventData)
-    {
-        Main.Get<UIManager>().ClosePopup();
-    }
+
 }
---
+++ Assets/Scripts/UI/Scene/NightMain_SceneUI.cs
@@ -0,0 +1,76 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class NightMain_SceneUI : BaseUI
+{
+    private Image _backPanel;
+
+    private Button _settingBtn;
+    private Button _playBtn;
+    private Button _x2SpeedBtn;
+    private Button _pausedBtn;
+
+    private TMP_Text _playerMoneyText;
+    private TMP_Text _stageText;
+
+    private Slider _hpSlider;
+
+    protected override void Init()
+    {
+        SetUI<Image>();
+        SetUI<Button>();
+        SetUI<TMP_Text>();
+        SetUI<Slider>();
+
+        _backPanel = GetUI<Image>("NightMainBackPanel");
+        _settingBtn = GetUI<Button>("NightMainSettingButton");
+        _playBtn = GetUI<Button>("PlayButton");
+        _x2SpeedBtn = GetUI<Button>("X2SpeedButton");
+        _pausedBtn = GetUI<Button>("PausedButton");
+        _playerMoneyText = GetUI<TMP_Text>("NightPlayerMoneyTxt");
+        _stageText = GetUI<TMP_Text>("NightCurStageTxt");
+        _hpSlider = GetUI<Slider>("HP_Slider");
+
+        SetUICallback(_settingBtn.gameObject, EUIEventState.Click, ClickSettingBtn);
+        SetUICallback(_playBtn.gameObject, EUIEventState.Click, ClickPlayBtn);
+        SetUICallback(_x2SpeedBtn.gameObject, EUIEventState.Click, ClickMultiplySpeedBtn);
+        SetUICallback(_pausedBtn.gameObject, EUIEventState.Click, ClickPausedBtn);
+
+    }
+
+    private void ClickSettingBtn(PointerEventData eventData)
+    {
+        Time.timeScale = 0.0f;
+        Main.Get<UIManager>().OpenPopup< Setting_PopupUI>("Setting_PopupUI");
+    }
+
+    private void ClickPlayBtn(PointerEventData eventData)
+    {
+        _playBtn.gameObject.SetActive(false);
+        _x2SpeedBtn.gameObject.SetActive(true);
+        _pausedBtn.gameObject.SetActive(false);
+        // todo ; 플레이 속도 1배속 -> 2배속으로 설정
+
+    }
+    private void ClickMultiplySpeedBtn(PointerEventData eventData)
+    {
+        _playBtn.gameObject.SetActive(false);
+        _x2SpeedBtn.gameObject.SetActive(false);
+        _pausedBtn.gameObject.SetActive(true);
+        // todo ; 플레이 속도 2배속 -> 일시정지로 설정
+
+    }
+    private void ClickPausedBtn(PointerEventData eventData)
+    {
+        _playBtn.gameObject.SetActive(true);
+        _x2SpeedBtn.gameObject.SetActive(false);
+        _pausedBtn.gameObject.SetActive(false);
+        // todo ; 플레이 속도 일시정지 -> 1배속으로 설정
+
+    }
+
+
+
+}
