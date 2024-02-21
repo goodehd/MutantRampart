f98e6d06fb6d7a4661d3f726f128f7e5f265427d
Chhhhong <101609335+Chhhhong@users.noreply.github.com>
2024-02-21 16:47:32 +0900

    [Add] StartScene Dotween
    스타트씬에서 아무곳이나 클릭하면 게임시작

--- Assets/Scripts/UI/Scene/StartScene_SceneUI.cs
+++ Assets/Scripts/UI/Scene/StartScene_SceneUI.cs
@@ -1,54 +1,27 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using System;
 using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
 
 public class StartScene_SceneUI : BaseUI
 {
-    private Button startNewGameBtn;
-    private Button continueGameBtn;
-    private Button settingBtn;
-    private Button endGameBtn;
-
+    private TextMeshProUGUI _pressAnyKeyTxt;
+    private float blinkDuration = 0.5f;
     protected override void Init()
     {
-        SetUI<Button>();
+        SetUI<TextMeshProUGUI>();
 
-        startNewGameBtn = GetUI<Button>("StartNewBtn");
-        continueGameBtn = GetUI<Button>("ContinueBtn");
-        settingBtn = GetUI<Button>("SettingBtn");
-        endGameBtn = GetUI<Button>("EndGameBtn");
+        _pressAnyKeyTxt = GetUI<TextMeshProUGUI>("PressAnyKeyTxt");
 
-        SetUICallback(startNewGameBtn.gameObject, EUIEventState.Click, ClickStartNewBtn);
-        SetUICallback(continueGameBtn.gameObject, EUIEventState.Click, ClickContinueGameBtn);
-        SetUICallback(settingBtn.gameObject, EUIEventState.Click, ClickSettingNewBtn);
-        SetUICallback(endGameBtn.gameObject, EUIEventState.Click, ClickEndGameBtn);
+        _pressAnyKeyTxt.DOBlendableColor(new Color(1, 1, 1, 0), blinkDuration).SetLoops(-1, LoopType.Yoyo);
     }
 
-    private void ClickStartNewBtn(PointerEventData data)
+    private void Update()
     {
-        Time.timeScale = 1.0f;
-        Main.Get<SceneManager>().ChangeScene<HongTestScene>();
-
-        // 기존에 데이터가 있는데 새로하기 버튼 누르면 확인 창 띄우기
-    }
-
-    private void ClickContinueGameBtn(PointerEventData data)
-    {
-        Time.timeScale = 1.0f;
-        Main.Get<SceneManager>().ChangeScene<HongTestScene>();
-        // 저장된 데이터 없이 이어하기 누른 경우 나중에 생각해주기
-    }
-
-    private void ClickSettingNewBtn(PointerEventData data)
-    {
-        Main.Get<UIManager>().OpenPopup<Setting_PopupUI>();
-    }
-
-    private void ClickEndGameBtn(PointerEventData data)
-    {
-        Application.Quit();
+        if(Input.anyKeyDown || Input.GetMouseButtonDown(0))
+        {
+            Main.Get<SceneManager>().ChangeScene<SelectScene>();
+        }
     }
 }
